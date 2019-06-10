9093e185b53bcb94a03ae4082b10ced12cb44dfd
FlyingDutchman1007 <38551439+FlyingDutchman1007@users.noreply.github.com>
2019-06-10 09:28:45 +0800

    update mybatis class and loggings
    为beta添加mybatis测试类

--- src/main/java/com/loan/easyloan/EasyLoanApplication.java
+++ src/main/java/com/loan/easyloan/EasyLoanApplication.java
@@ -3,6 +3,7 @@ package com.loan.easyloan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//主入口类，标注在某个类上，说明这个类是SpringBoot的主配置类
 @SpringBootApplication
 public class EasyLoanApplication {
 
